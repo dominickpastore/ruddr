bd4f55cd6c428cc912f302ddcbe292b4a3b50ccd
Dominick C. Pastore <dominickpastore@dcpx.org>
2022-12-03 12:00:38 -0500

    Set version automatically

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
+import subprocess
+
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
+# Get version from "git describe" (requires releases to be tagged vX.Y.Z and
+# initial commit to be tagged v0.0.0, both with annotated tags)
+git_version = subprocess.check_output(
+    ['git', 'describe', '--abbrev', '--dirty'],
+    text=True,
+)
+version_parts = git_version.strip().lstrip('v').split('-')
+version = version_parts[0]
+if len(version_parts) > 1:
+    version += f".dev{version_parts[1]}+{version_parts[2]}"
+if len(version_parts) > 3:
+    version += ".dirty"
+
 setup(
     name='ruddr',
-    version='0.0.0dev0',
+    version=version,
     author='Dominick C. Pastore',
     author_email='ruddr@dcpx.org',
     description='Robotic Updater for Dynamic DNS Records',
