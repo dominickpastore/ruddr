14a3d84896b15d3a54aa21bbab57849b4d2938d7
Dominick C. Pastore <dominickpastore@dcpx.org>
2023-01-15 22:40:18 -0500

    Add tests for updater/notifier validators

--- test/conftest.py
+++ test/conftest.py
@@ -1,8 +1,9 @@
+import threading
 from typing import List
 
 import pytest
 
-import threading
+import doubles
 
 
 class VirtualTimer:
@@ -119,3 +120,37 @@ def advance():
     yield advancer
 
     threading.Timer = orig_timer
+
+
+@pytest.fixture
+def notifier_factory():
+    """Fixture creating a factory for fake :class:`~ruddr.Notifier`"""
+    class NotifierFactory:
+        def __init__(self):
+            self._count = 0
+
+        def __call__(self, **kwargs):
+            self._count += 1
+            config = kwargs
+            return doubles.FakeNotifier(f'fake_notifier_{self._count}', config)
+    return NotifierFactory()
+
+
+@pytest.fixture
+def updater_factory():
+    """Fixture creating a factory for mock :class:`~ruddr.Updater`"""
+    class UpdaterFactory:
+        def __init__(self):
+            self._count = 0
+
+        def __call__(self, **kwargs):
+            self._count += 1
+            return doubles.MockBaseUpdater(f'mock_updater_{self._count}')
+    return UpdaterFactory()
+
+
+@pytest.fixture
+def mock_updater(updater_factory):
+    """Fixture creating a mock :class:`~ruddr.Updater` that keeps a list of
+    IP addresses it receives"""
+    return updater_factory()
--- test/test_manager.py
+++ test/test_manager.py
@@ -1,7 +1,84 @@
-# import ruddr
+import ruddr.manager
+
+import pytest
 
 
-# TODO Test notifier not attached to updaters is discarded
+@pytest.mark.parametrize('expected', [True, False])
+def test_validate_notifier_type(mocker, notifier_factory, expected):
+    """Test validate_notifier_type calls _validate_updater_or_notifier_type
+    with 'notifier'"""
+    validator = mocker.patch(
+        "ruddr.manager._validate_updater_or_notifier_type",
+        return_value=expected,
+    )
+    notifiers = {
+        'test': notifier_factory(),
+    }
+    mocker.patch("ruddr.manager.notifiers.notifiers", new=notifiers)
+
+    result = ruddr.manager.validate_notifier_type('testmod', 'testtype')
+
+    assert result == expected
+    assert validator.call_args == (
+        ("notifier", notifiers, 'testmod', 'testtype'),
+    )
 
 
-# TODO Test notifier only attached for skipped address family is discarded
+@pytest.mark.parametrize('expected', [True, False])
+def test_validate_updater_type(mocker, updater_factory, expected):
+    """Test validate_updater_type calls _validate_updater_or_notifier_type
+    with 'updater'"""
+    validator = mocker.patch(
+        "ruddr.manager._validate_updater_or_notifier_type",
+        return_value=expected,
+    )
+    updaters = {
+        'test': updater_factory(),
+    }
+    mocker.patch("ruddr.manager.updaters.updaters", new=updaters)
+
+    result = ruddr.manager.validate_updater_type('testmod', 'testtype')
+
+    assert result == expected
+    assert validator.call_args == (
+        ("updater", updaters, 'testmod', 'testtype'),
+    )
+
+
+# TODO Add tests for _validate_updater_or_notifier_type. It can do updaters
+#  only since the behavior is exactly the same for notifiers.
+# TODO Type is built-in updater
+# TODO Type is entry_point updater (added to updaters dict)
+# TODO Type is built-in updater but entry_point with same name exists, built-in
+#  takes priority
+# TODO Type does not match built-in or entry_point updater
+# TODO Module and type is importable (added to updaters dict)
+# TODO Module and type is not importable
+# TODO Module and type already in updaters dict
+# TODO Module and type is importable, type matches built-in (added to updaters
+#  dict)
+# TODO Module and type already in updaters dict, type matches built-in
+
+
+# TODO Manager creates notifier with type only and passes in name and config
+# TODO Manager creates notifier with module and type and passes in name and
+#  config
+# TODO Manager creates updater with type only and passes in name, config, and
+#  addrfile
+# TODO Manager creates updater with module and type and passes in name, config,
+#  and addrfile
+
+
+# TODO Notifier is attached to updater with notifier4
+# TODO Notifier is attached to updater with notifier6
+# TODO Notifier is attached to updater with notifier4 and notifier6
+# TODO Notifier is attached to different updaters with notifier4 and notifier6
+
+
+# TODO Test notifier that doesn't want IPv4 or IPv6 is not started with start
+# TODO Test notifier that doesn't want IPv4 or IPv6 is not stopped with stop
+# TODO Test notifier that doesn't want IPv4 or IPv6 is not called with
+#  do_notify
+
+
+# TODO Notifiers are stopped after one raises NotifierSetupError
